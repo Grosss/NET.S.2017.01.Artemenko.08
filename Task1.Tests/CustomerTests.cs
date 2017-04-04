cb5cf251d0e77134367740f5472a99bd45a2da26
Grosss <artemenko,nickolai@gmail.com>
2017-04-04 17:55:13 +0200

    tests are added

--- Task1.Tests/CustomerTests.cs
+++ Task1.Tests/CustomerTests.cs
@@ -11,11 +11,48 @@ namespace Task1.Tests
     [TestFixture]
     public class CustomerTests
     {
-        [Test]
-        public void TestMethod()
+        [TestCase("G", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("NRP", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("NR", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00")]
+        [TestCase("N", ExpectedResult = "Customer record: Jeffrey Richter")]
+        [TestCase("R", ExpectedResult = "Customer record: 1,000,000.00")]
+        [TestCase("P", ExpectedResult = "Customer record: +1 (425) 555-0100")]
+        [TestCase(null, ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        public string ToString_PassedStringFormatAndFormatProvider_ExpectedPositiveTest(string format)
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            return customer.ToString(format, CultureInfo.InvariantCulture);
+        }
+
+        [TestCase("G", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("NRP", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("NR", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00")]
+        [TestCase("N", ExpectedResult = "Customer record: Jeffrey Richter")]
+        [TestCase("R", ExpectedResult = "Customer record: 1,000,000.00")]
+        [TestCase("P", ExpectedResult = "Customer record: +1 (425) 555-0100")]
+        [TestCase(null, ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        [TestCase("", ExpectedResult = "Customer record: Jeffrey Richter, 1,000,000.00, +1 (425) 555-0100")]
+        public string ToString_PassedStringFormat_ExpectedPositiveTest(string format)
+        {
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            return customer.ToString(format);
+        }
+
+        [TestCase("N02")]
+        [TestCase("F")]
+        public void ToString_PassedInvalidStringFormatAndFormatProvider_ExpectedPositiveTest(string format)
+        {
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            Assert.Throws<FormatException>(() => customer.ToString(format, CultureInfo.InvariantCulture));
+        }
+
+        [TestCase("N02")]
+        [TestCase("F")]
+        public void ToString_PassedInvalidStringFormat_ExpectedPositiveTest(string format)
+        {
+            Customer customer = new Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            Assert.Throws<FormatException>(() => customer.ToString(format));
         }
     }
 }
--- Task1/Customer.cs
+++ Task1/Customer.cs
@@ -49,36 +49,52 @@ namespace Task1
 
         #region String representation methods
 
+        /// <summary>
+        /// Converts fields of this instance to a specific string representation    
+        /// </summary>
+        /// <returns>Customer's string representation</returns>
         public override string ToString()
         {
             return $"Customer record: {Name} {Revenue.ToString("N02", CultureInfo.InvariantCulture)} {ContactPhone}"; 
         }
 
+        /// <summary>
+        /// Converts fields of this instance to a specific string representation 
+        /// </summary>
+        /// <param name="format">A custom format string</param>
+        /// <returns>Customer's string representation</returns>
         public string ToString(string format)
         {
             return ToString(format, CultureInfo.InvariantCulture);
         }
 
+        /// <summary>
+        /// Converts fields of this instance to a specific string representation 
+        /// </summary>
+        /// <param name="format">A custom format string</param>
+        /// <param name="formatProvider">An object that supplies culture-specific formatting information</param>
+        /// <returns>Customer's string representation</returns>
+        /// <exception cref="FormatException">The format string is not supported</exception>
         public string ToString(string format, IFormatProvider formatProvider)
         {
             if (string.IsNullOrEmpty(format)) format = "G";
 
-            if (formatProvider == null)
+            if (ReferenceEquals(formatProvider, null))
                 formatProvider = CultureInfo.InvariantCulture;
 
             switch (format.Trim().ToUpperInvariant())
             {
                 case "G":
                 case "NRP":
-                    return $"Customer record: {Name} {Revenue.ToString("N02", formatProvider)} {ContactPhone}";
+                    return $"Customer record: {Name}, {Revenue.ToString("N02", formatProvider)}, {ContactPhone}";
                 case "NR":
-                    return $"Customer record: {Name} {Revenue.ToString("N02", formatProvider)}";
+                    return $"Customer record: {Name}, {Revenue.ToString("N02", formatProvider)}";
                 case "N":
                     return $"Customer record: {Name}";
                 case "R":
-                    return Revenue.ToString("N02", formatProvider);
+                    return $"Customer record: {Revenue.ToString("N02", formatProvider)}";
                 case "P":
-                    return ContactPhone;
+                    return $"Customer record: {ContactPhone}";
                 default:
                     throw new FormatException($"The {format} format string is not supported");
             }            
